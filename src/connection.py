f405f6373c6bbd29ae5669868b1cbe5ba172cba0
Iván Andrés Yagual Méndez <ivandres000@gmail.com>
2026-04-12 00:55:13 +0200

    add comments

--- src/connection.py
+++ src/connection.py
@@ -8,25 +8,24 @@ try:
     rtde_r = rtde_receive.RTDEReceiveInterface(IP_ROBOT)
     rtde_c = rtde_control.RTDEControlInterface(IP_ROBOT)
 
-    # 1. Esperamos a que el robot esté quieto
-    print("Esperando estabilización...")
+    # 1. Waiting
+    print("Waiting update...")
     time.sleep(1)
 
-    # 2. Leemos la posición REAL actual
+    # 2. Reading actual position
     pose_leida = rtde_r.getActualTCPPose()
-    print(f"TCP LEÍDO -> X: {pose_leida[0]:.4f}, Y: {pose_leida[1]:.4f}, Z: {pose_leida[2]:.4f}")
+    print(f"TCP readed -> X: {pose_leida[0]:.4f}, Y: {pose_leida[1]:.4f}, Z: {pose_leida[2]:.4f}")
 
-    # 3. Intentamos movernos a esa MISMA posición
-    # Usamos moveL (Lineal) con una velocidad baja para observar
-    print("Enviando orden de movimiento al mismo punto...")
+    # 3. Movement
+    print("Sending movement order...")
     rtde_c.moveL(pose_leida, speed=0.1, acceleration=0.05)
 
-    # 4. Volvemos a leer tras el movimiento
+    # 4. We reading TCP again
     time.sleep(1)
     nueva_pose = rtde_r.getActualTCPPose()
     print(f"TCP FINAL -> X: {nueva_pose[0]:.4f}, Y: {nueva_pose[1]:.4f}, Z: {nueva_pose[2]:.4f}")
 
-    # 5. Calculamos la diferencia (El error)
+    # 5. Calculate error
     error_x = abs(pose_leida[0] - nueva_pose[0])
     print(f"Error detectado en X: {error_x * 1000:.2f} mm")
 
