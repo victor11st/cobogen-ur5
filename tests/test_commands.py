f267b87f9b9c563ea4f5c2095152854aff7036bc
Iván Andrés Yagual Méndez <ivandres000@gmail.com>
2026-04-12 00:56:40 +0200

    feach: Test move robot.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,2 +1,15 @@
-def test_placeholder():
-    assert True
+
+from src.ur5 import UR5
+
+def main():
+    robot = UR5(ip='172.17.0.2', 
+                tools={"pinza":(0.0, 0.0, 100, 0.0, 0.0, 0.0)},
+                workplace=(0.0, 0.0, 0.0, 0.0), 
+                home=(0, -60, -130, 13, 90, 10))
+    
+    robot.move_to_home()
+
+
+
+if __name__ == "__main__":
+    main()
