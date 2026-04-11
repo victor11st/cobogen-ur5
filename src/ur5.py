6243ffc6a66e1df0a417544cab3a92f8c6ed897d
Iván Andrés Yagual Méndez <ivandres000@gmail.com>
2026-04-12 00:44:07 +0200

    feach: Add routine execution and measurement updates

--- src/ur5.py
+++ src/ur5.py
@@ -3,19 +3,24 @@ import rtde_control
 import math
 
 class UR5():
-<<<<<<< Updated upstream
     "Robot UR5"
-    def __init__(self, ip: str, tools: dict["name":"tcp"], workplace: list[float], home: tuple[float], freedom_grades: int):
-        tools = UR5._comprobe_tool(tools)
+    _JOINT_LIMIT_DEG = 355
+
+    def __init__(self, ip: str, tools: dict[str:tuple[int]], workplace: list[float], home: tuple[float], sequence):
+        tools = UR5._validate_tools(tools)
+        self._home = UR5._validate_home(home)
+
+        # Interfaces
         self._receptor = rtde_receive.RTDEReceiveInterface(ip)
         self._controlador = rtde_control.RTDEControlInterface(ip)
+
         self._workplace = workplace
         self._tools = tools
         self._active_tool = None
         self._estate = None
         self._tcp = None
-        #self._home =  UR5._validate_home(home, freedom_grades)
-        self._home = (0, -60, -130, 13, 90, 10)
+        self._sequence = sequence
+
 
     @property
     def home(self):
@@ -25,95 +30,83 @@ class UR5():
     def home(self, new_home: str):
         self._home = UR5._validate_home(new_home)
 
+    @property
+    def active_tool(self):
+        return self.active_tool
+
+
     @staticmethod
     def _validate_home(home: tuple[float]):
         for grades_motor in home:
-            if not isinstance(grades_motor, float) and 350 < abs(grades_motor) < 0:
-                print("Grades of motor aren't invalids.")
+            if abs(grades_motor) > UR5._JOINT_LIMIT_DEG:
                 return None
-        return home
+        return UR5._deg_to_rad(home)
+
 
     @staticmethod
-    def _validate_tool(tools: dict["name":"tcp"]):
-        validate = False
-        tools_validates = {}
-        for name, tcp in tools.items():
-            if len(tcp) == 6:
-                for coord in tcp:
-                    if isinstance(coord, int):
-                        validate = True
-                    tools_validates[name] = tcp
+    def _validate_tools(tools: dict[str:tuple[int]]):
+        for name, tcp in tools.intems():
+            if len(tcp) != 6:
+                tool_deleted = tools.pop(name)
+                print(f"{tool_deleted} was ignored: Values incorrect.")        
+        return tools
 
-            if validate == True:
-                    print(f"{name} was add sucesful.")
-                    tools_validates[name] = tcp
-            else:
-                print(f"{name} was ignored, coordenates invalids.")
-        return tools_validates
-
-=======
-    def __init__(self, ip: str, tools: dict["name":"tcp"]):
-        UR5._comprobe_tool(tools)
-        self._receptor = rtde_receive.RTDEReceiveInterface(ip)
-        self._controlador = rtde_control.RTDEControlInterface(ip)
-        self._entorno = None
-        self._tools = tools
-        self._active_tool = None
-        self._estadp = None
-        self._tcp = None
-        self._home =  None
-
-
-    
     @staticmethod
-    def _comprobe_tool(tools: dict["name":"tcp"]):
-        if len(TCP) == 6:
-            return True
-        return False
+    def _deg_to_rad(values: tuple[float|int]) -> list[float]:
+        return tuple(math.radians(d) for d in values)
 
->>>>>>> Stashed changes
+    @staticmethod
+    def _mm_to_m(value):
+        return [mm/1000.0 for mm in value]
 
-
-    def add_tool(self, tool: str, TCP: tuple[float]):
-        if UR5._comprobe_tool(TCP):
-            self._tools[tool] = TCP
-        else:
-<<<<<<< Updated upstream
-            print("Tool wasn't add: Coordenates of TCP are invalids")
-=======
-            print("Tool wasn't add: TCPenates of TCP aren validates")
->>>>>>> Stashed changes
+    def add_tool(self, tools: dict[str:tuple]):
+        tools = self._validate_tools(tools)
+        if tools:
+            for name, tcp in tools:
+                self._tools[name] = tcp
 
 
-    def change_tool(self, name_tool):
-        if name_tool not in self.herramientas:
-            print("Tool not defined")
-            return False
-
-        new_tcp = self.herramientas[name_tool] 
-        self.controlador.setTcp(new_tcp)
-
+    def change_active_tool(self, name_tool):
+        self.controlador.setTcp(self.herramientas[name_tool] )
         self.active_tool = name_tool
 
         print("The tool active is:", self.active_tool)
-        return True
     
-    def consult_tool_activate(self):
-        print("Tool active:", self._active_tool)
 
 
     def move_to_home(self):
-<<<<<<< Updated upstream
-        self._controlador.movej(self.home)
-=======
-        pass
->>>>>>> Stashed changes
+        self._controlador.moveJ(self._home)
 
+
+    def execute_routine(self, name_routine:str):
+        routine = self._sequence["routines"][name_routine]
+        points = self._sequence["points"]
+        for target in routine:
+            name = target["name"]
+            type_mov = target.get("type", "joint")
+            speed = target.get("speed", 0.5)
+            acceleration = target.get("acceleration", 1.0)
+            
+            aux = points[name]
+
+            point = aux["position"] + aux["orientation"]
+
+            if type_mov == "joint":
+                self._controlador.moveJ_IK(point, speed, acceleration)
+
+            elif type_mov == "linear":
+                self._controlador.moveL(point, speed, acceleration)
+  
+
+
+
+
+
+            
         
-    def pick_and_place(self):
-        pass
 
-    def emergency_stop():
+
+    def teach_mode():
         pass
 
 
