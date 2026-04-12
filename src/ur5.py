8214529682a5fb947b3264d2976d0a7faf481ea2
Iván Andrés Yagual Méndez <ivandres000@gmail.com>
2026-04-12 19:08:22 +0200

    feat: Add methods for add new pints , delete tool and minor changes

--- src/ur5.py
+++ src/ur5.py
@@ -1,38 +1,66 @@
 import rtde_receive
 import rtde_control
 import math
+import json
 
 class UR5():
     "Robot UR5"
     _JOINT_LIMIT_DEG = 355
 
-    def __init__(self, ip: str, tools: dict[str:tuple[int]], workplace: list[float], home: tuple[float], sequence):
+    def __init__(self, ip: str, tools: dict[str:tuple[int]], workplace: list[float], home: tuple[float], path_json: str):
         tools = UR5._validate_tools(tools)
         self._home = UR5._validate_home(home)
+        
 
         # Interfaces
         self._receptor = rtde_receive.RTDEReceiveInterface(ip)
         self._controlador = rtde_control.RTDEControlInterface(ip)
 
+        full_data = UR5.laod_from_json(path_json)
         self._workplace = workplace
         self._tools = tools
         self._active_tool = None
         self._estate = None
         self._tcp = None
-        self._sequence = sequence
+        self._points = full_data.get("points", {})
+        self._routines = full_data.get("routines", {})
+        self._auto_point_counter = 0 # Adapter for JSON
 
-
+#-----------| GETTERS |-------------------
     @property
     def home(self):
         return self._home
+
+    @property
+    def available_routines(self):
+        return list(self._routines.keys())
     
-    @home.setter
-    def home(self, new_home: str):
-        self._home = UR5._validate_home(new_home)
+    @property
+    def available_points(self):
+        return list(self._points.keys())
+
 
     @property
     def active_tool(self):
         return self.active_tool
+#-------------------------------------------
+
+#----------| SETTERS |-----------------------
+    @home.setter
+    def home(self, new_home: str):
+        self._home = UR5._validate_home(new_home)
+#------------------------------------------
+
+    @staticmethod
+    def laod_from_json(json_path:str):
+        try:
+            with open(json_path, 'r') as f:
+                full_data = json.load(f)
+            return full_data
+        except FileNotFoundError:
+            print("Fichero no encontrado")
+        except json.JSONDecodeError:
+            print(f"Error: El archivo JSON tiene un formato incorrecot")
 
 
     @staticmethod
@@ -60,13 +88,23 @@ class UR5():
         return [mm/1000.0 for mm in value]
 
     def add_tool(self, tools: dict[str:tuple]):
+        """
+        Add news tools
+        """
         tools = self._validate_tools(tools)
         if tools:
             for name, tcp in tools:
                 self._tools[name] = tcp
 
+    def delete_tool(self, name_tool):
+        tool = self._tools.pop(name_tool)
+        print(f"Se eliminó {tool} correctamente.")
+
 
     def change_active_tool(self, name_tool):
+        """
+        Change tool.
+        """
         self.controlador.setTcp(self.herramientas[name_tool] )
         self.active_tool = name_tool
 
@@ -75,12 +113,18 @@ class UR5():
 
 
     def move_to_home(self):
+        """
+        Move the robot at the home position
+        """
         self._controlador.moveJ(self._home)
 
 
-    def execute_routine(self, name_routine:str):
-        routine = self._sequence["routines"][name_routine]
-        points = self._sequence["points"]
+    def execute_routine(self, name_routine:str) -> None:
+        """
+        Execute routine.
+        """
+        routine = self._routines[name_routine]
+        points = self._points
         for target in routine:
             name = target["name"]
             type_mov = target.get("type", "joint")
@@ -97,19 +141,37 @@ class UR5():
             elif type_mov == "linear":
                 self._controlador.moveL(point, speed, acceleration)
   
+    def add_routine(self, new_points:list[dict], name_routine:str):
+        name_point = self.add_point()
+        self._routines[name_routine].append({
+            "name":name_point,
+            "type":"",
+            "speed":0.8,
+            "acceleration":2
+        })
+
+    def add_point(self, name_point:str, position:str, orientation:str, config:dict):
+        coord_point = self._receptor.getActualPose()
+        if name_point == None:
+            name_point == f"point_{self._auto_point_counter}" #Generate auto name to point
+        position = coord_point[:3]
+        orientation = coord_point[3:]
+
+        new_point = {
+            "position":position,
+            "orientation":orientation 
+        }
+    
+        # conf_new_point{
+        #     "name":name_point,
+        #     "type":config[type_mov],
+        #     "speed":config[speed],
+        #     "aceceleration":config[acceleration]
+        # }
 
 
-
-
-
+        self._points[name_point] = new_point 
+        return new_point
             
-        
-
-
-    def teach_mode():
-        pass
-
-
-
-
+            
 
